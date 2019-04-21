f91701f868e761264d488fdcf5addd8bdd3c724e
whosAle <abelgrave95@gmail.com>
2019-04-21 16:38:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i}.`);
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  let streak = 0;
+
+  while (Math.random() >= 0.5) {
+    streak++;
+  }
+
+  console.log(`You got ${streak} tails in a row!`);
+
+}
