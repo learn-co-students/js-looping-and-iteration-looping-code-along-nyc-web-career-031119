e1bb22cf9ef0496557a89a947153d8b68c30f31a
Theo Weller <weller.theodor@gmail.com>
2019-04-22 21:13:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+const myBag = ["Hella papes", "Supreme Brick", "Live mice"];
+
+for(let x = 0; myBag.length > x; x++) {
+  console.log(myBag[x])
+}
+
+let countdown = 20;
+
+while (countdown > 0) {
+  console.log(countdown--)
+}
